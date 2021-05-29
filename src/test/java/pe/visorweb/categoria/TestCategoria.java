1f114da2204f88231a1295bc9d34b8dc6d4b4a03
Fanely <55720797+alunaufginf@users.noreply.github.com>
2021-05-29 18:08:01 -0500

    Update TestCategoria.java
    version2

--- src/test/java/pe/visorweb/categoria/TestCategoria.java
+++ src/test/java/pe/visorweb/categoria/TestCategoria.java
@@ -34,6 +34,33 @@ public class TestCategoria
             txtClave.clear();
             txtClave.sendKeys("clave");
 
+            WebElement btnIniciarSesion = driver.findElement(By.id("btnIniciarSesion"));
+            btnIniciarSesion.click();
+
+            //clic en el boton hamburguesa
+            Thread.sleep(2000); //una demora de dos segundos
+            driver.findElement(By.xpath("/html/body/section/div[1]/div")).click();
+            Thread.sleep(1000);
+            driver.findElement(By.xpath("/html/body/section/div[1]/nav/ul/li/span")).click();
+            Thread.sleep(1000);
+            driver.findElement(By.linkText("Mnt. de Categoría")).click();
+            driver.findElement(By.id("btnNuevo")).click();
+            driver.findElement(By.id("txtNombre")).clear();
+            driver.findElement(By.id("txtNombre")).sendKeys("Selenium Web Driver2");
+            driver.findElement(By.id("btnGuardar")).click();
+
+            Thread.sleep(2000);
+
+            String mensajeEsperado= "Se guardó de manera correcta la Categoría";
+            String mensajeObtenido= driver.findElement(By.id("messages")).getText();
+
+            //Es un punto de verificación, si son iguales los mensajes el TEST PASA
+            assertEquals(mensajeEsperado, mensajeObtenido);
+
+            driver.close();
+
+
+
         }catch (Exception e){
             e.printStackTrace();
             fail();
@@ -41,6 +68,54 @@ public class TestCategoria
     }
     @Test
     public void testInsertarCategoriaDatosVacios(){
-        System.out.println("Se ejecuta método insertarCategoriadatosvacios ");
+        try{
+            //1. configurar el driver
+            WebDriverManager.firefoxdriver().setup();
+            //lo descarga y lo enlaza al proyecto para poder usarlo
+            //2- instanciamos el driver
+            driver = new FirefoxDriver();
+            //3. maximizar el navegador
+            driver.manage().window().maximize();
+
+            //1.  cargar página visorweb
+            driver.get(url);
+            driver.findElement(By.id("txtUsuario")).clear();
+            driver.findElement(By.id("txtUsuario")).sendKeys("admin");
+
+            WebElement txtClave = driver.findElement(By.id("txtClave"));
+            txtClave.clear();
+            txtClave.sendKeys("clave");
+
+            WebElement btnIniciarSesion = driver.findElement(By.id("btnIniciarSesion"));
+            btnIniciarSesion.click();
+
+            //clic en el boton hamburguesa
+            Thread.sleep(2000); //una demora de dos segundos
+            driver.findElement(By.xpath("/html/body/section/div[1]/div")).click();
+            Thread.sleep(1000);
+            driver.findElement(By.xpath("/html/body/section/div[1]/nav/ul/li/span")).click();
+            Thread.sleep(1000);
+            driver.findElement(By.linkText("Mnt. de Categoría")).click();
+            driver.findElement(By.id("btnNuevo")).click();
+            driver.findElement(By.id("txtNombre")).clear();
+         //   driver.findElement(By.id("txtNombre")).sendKeys("");
+            driver.findElement(By.id("btnGuardar")).click();
+
+            Thread.sleep(2000);
+
+            String mensajeEsperado= "Nombre: Error de validación: se necesita un valor.";
+            String mensajeObtenido= driver.findElement(By.id("messages")).getText();
+
+            //Es un punto de verificación, si son iguales los mensajes el TEST PASA
+            assertEquals(mensajeEsperado, mensajeObtenido);
+
+            driver.close();
+
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+            fail();
+        }
     }
 }
